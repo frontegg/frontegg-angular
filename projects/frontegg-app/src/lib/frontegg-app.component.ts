47210915a5d56c7b1cb248c8a6ec237ffc8bcee8
Arnaut Max <maximarnayt@gmail.com>
2021-06-03 15:03:36 +0300

    FR-3126: prevent ng-content if isLoading/isAuthRoute false

--- projects/frontegg-app/src/lib/frontegg-app.component.ts
+++ projects/frontegg-app/src/lib/frontegg-app.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FronteggAppService } from './frontegg-app.service';
 @Component({
   selector: 'lib-frontegg-app',
-  template: ``,
+  template: `<div *ngIf="!loading && !isAuthRoute"><ng-content></ng-content></div>`,
 })
 export class FronteggAppComponent implements OnInit, OnDestroy {
   name: string;
+  loading: boolean;
+  isAuthRoute: boolean;
+
   constructor(private fronteggAppService: FronteggAppService) {
     this.name = 'FronteggApp';
-  }
-
-  ngAfterViewInit(): void {
-    this.fronteggAppService?.fronteggApp?.mountAdminPortal()
+    this.loading = false;
+    this.isAuthRoute = false;
   }
 
   ngOnDestroy(): void {
     this.fronteggAppService?.fronteggApp?.unmountAdminPortal()
   }
 
-
   ngOnInit(): void {
+    this.fronteggAppService.isLoading$.subscribe((loading) => {
+      this.loading = loading
+    })
+    this.fronteggAppService.isAuthRoute$.subscribe((isAuthRoute) => {
+      this.isAuthRoute = isAuthRoute
+    })
   }
 
 }
