9afbceaa80bb950a544000e54949a771f5b48a45
Piotr Pliszczyński <piotr.pilczynski@frontegg.com>
2022-10-04 14:52:25 +0200

    Initial

---
+++ projects/sanity-check/app.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {FronteggAppService, FronteggAuthService} from "@frontegg/angular";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit, OnDestroy {
+  isLoading = true;
+  loadingSubscription: Subscription;
+  user?: any;
+
+  constructor(private fronteggAuthService: FronteggAuthService, private fronteggAppService: FronteggAppService) {
+    this.loadingSubscription = fronteggAppService.isLoading$.subscribe((isLoading) => this.isLoading = isLoading)
+  }
+
+  ngOnInit(): void {
+    this.fronteggAuthService?.user$.subscribe((user) => {
+      this.user = user
+    })
+  }
+
+  showAdminPortal(): void {
+    this.fronteggAppService?.showAdminPortal()
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSubscription.unsubscribe()
+  }
+}
