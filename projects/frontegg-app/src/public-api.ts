0acf6142d9802b19df815e3fc8c7d7e97ee7acea
Yuval Lotem <yuval.lotem@frontegg.com>
2023-11-27 16:32:09 +0200

    FR-14187 - support standalone components

--- projects/frontegg-app/src/public-api.ts
+++ projects/frontegg-app/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of frontegg-app
  */
 
-export { FronteggAppModule } from './lib/frontegg-app.module';
+export { FronteggAppModule, provideFrontegg } from './lib/frontegg-app.module';
 export { FronteggAppService } from './lib/frontegg-app.service';
 export { FronteggComponent } from './lib/frontegg.component';
 export { FronteggAuthService } from './lib/frontegg-auth.service';
